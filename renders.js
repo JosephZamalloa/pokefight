b6551c43068e099d11e35c5a49e2705b667a6ec8
jzc1224 <josephzamalloa.c@gmail.com>
2022-02-02 00:10:10 -0500

    ya casi termina

--- renders.js
+++ renders.js
@@ -1,6 +1,13 @@
 
-function renderPoke(poke, targetElement)
+function renderPoke(poke, targetElement,pos)
 {   
+  let player
+    if(pos==1)
+    {
+      player="p1"
+    }else{
+      player="p2"
+    }
     let num=0
     const html = poke.map((pokemon)=>{
       num++
@@ -10,14 +17,14 @@ function renderPoke(poke, targetElement)
         }
         return `
           <div class="name-order" >  
-                <span class="boton-poke" id="${num}" onclick="selecciona()" >${(pokemon.name).charAt(0).toUpperCase()+(pokemon.name).slice(1)} ${num}
+                <span class="boton-poke" id="${num}" onclick="selecciona(${num},${player})" >${(pokemon.name).charAt(0).toUpperCase()+(pokemon.name).slice(1)} ${num}
                 <img src="https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/other/home/${num}.png?raw=true" width="190px">
                 </span>
           </div>
         `
     }).join('')
 targetElement.innerHTML =html
-console.log(pokemon)
+console.log(pos)
 }
 
 
--- scripts.js
+++ scripts.js
@@ -1,35 +1,42 @@
-
 let pokemon = []
 let n=0
-let ids
-
+var pos=0
 let modal=document.getElementById("modal_1")
 let btn_1= document.getElementById("p1")
 let btn_2= document.getElementById("p2")
 let cerrar=document.getElementById("close")
-
 let player_select=document.getElementById("choose")
-
 let poke_players=[]
-
-//este recibe a los pokemon de la api
-getPoke('pokemon/?limit=150')
-.then((pokeRes)=>{
-    pokemon=pokeRes
-    let poke_name=document.getElementById('poke-name')
-    renderPoke(pokemon,poke_name)
-
-})
 
 //Botones del modal
 btn_1.onclick = function(){
    modal.style.display="block"
+   pos=1
+    //este recibe a los pokemon de la api
+  getPoke('pokemon/?limit=150')
+  .then((pokeRes)=>{
+    pokemon=pokeRes
+    let poke_name=document.getElementById('poke-name')
+    renderPoke(pokemon,poke_name,pos)
+
+    })
     }
 
 btn_2.onclick = function(){
    modal.style.display="block"
-    }
+   pos=2
+    //este recibe a los pokemon de la api
+  getPoke('pokemon/?limit=150')
+  .then((pokeRes)=>{
+    pokemon=pokeRes
+    let poke_name=document.getElementById('poke-name')
+    renderPoke(pokemon,poke_name,pos)
 
+    })
+  }
+
+
+   
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none"
@@ -40,9 +47,19 @@ cerrar.onclick = function(){
     modal.style.display= "none"
 }
 
-function selecciona(){
+function selecciona(e,targetElement){
   modal.style.display= "none"
-
+  
+  const html = `
+          <div class="sel-player" >  
+                
+                <img src="https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/other/home/${e}.png?raw=true" width="300px">
+                </span>
+          </div>
+        `
+    
+targetElement.innerHTML =html
+  
   
 }
 
